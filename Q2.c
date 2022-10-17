7dc1be098caac991c9b3572a1925954d55c0825f
AtiendriyaV <65350839+AtiendriyaV@users.noreply.github.com>
2022-10-17 09:54:51 +0530

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+# define SIZE 100
+int main()
+{
+
+{
+printf("welcome to bank cash counter");
+}
+
+int a,A,S,n;
+
+for(a=0;a<n;a++){
+
+printf("enter the amount to be deposited:");
+scanf("%d",&a);
+
+printf("amount entered is: %d",a);
+
+
+printf("Display the transaction amount;");
+scanf("%d",&a);
+
+if(a=0)
+{
+S = ("amount entered is: %d",a) + 0;
+printf("sum of amount deposited  is  : %d" ,S);
+}
+else
+{
+S = ("amount entered is: %d",a) + a; 
+printf("Sum of amount is : %d",S);
+
+}
+}
+}
